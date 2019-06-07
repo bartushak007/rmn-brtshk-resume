b8c104aa5788c542b94850a1ef34d0e31ab0fe97
Roman <rmnbrtshk@gmail.com>
2019-06-07 13:19:15 +0300

    Updates

---
+++ precache-manifest.d450a5dcf9605e934a1dd35e4863289f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ff97aa600bc32be7037f77a411b80f9",
+    "url": "/rmn-brtshk-resume/index.html"
+  },
+  {
+    "revision": "5d7723d4bc31befd1a8a",
+    "url": "/rmn-brtshk-resume/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/rmn-brtshk-resume/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "5d7723d4bc31befd1a8a",
+    "url": "/rmn-brtshk-resume/static/js/main.c3b1e637.chunk.js"
+  },
+  {
+    "revision": "5c3d88bf613b289e1f8f",
+    "url": "/rmn-brtshk-resume/static/js/runtime~main.94fac41e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rmn-brtshk-resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
