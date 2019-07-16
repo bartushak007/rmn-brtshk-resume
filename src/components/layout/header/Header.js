36f479c5b97970f84b54f5b00c9eabe5abcc2882
Roman <rmnbrtshk@gmail.com>
2019-07-16 20:28:15 +0300

    Change Header update string

--- src/components/layout/header/Header.js
+++ src/components/layout/header/Header.js
@@ -5,7 +5,7 @@ function Header() {
     <header className="text-right text">
       <div className="container-wide">
         <h2 className="title title--uppercase">Welcome to my page!</h2>
-        <p>Last updated 19.06.2019</p>
+        <p>Last updated 16.07.2019</p>
       </div>
     </header>
   );
