216bc2f592223549bfd9f2d43b24f8b1fe480359
bartushak007 <rmnbrtshk@gmail.com>
2019-10-09 16:18:26 +0300

    Updates

---
+++ precache-manifest.81dd694af416a8c32f10744d7d94bd77.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d9d3822491e4c148d842d612a675cb7",
+    "url": "/rmn-brtshk-resume/index.html"
+  },
+  {
+    "revision": "2e6bffa6107ad3258e68",
+    "url": "/rmn-brtshk-resume/static/css/3.869881bd.chunk.css"
+  },
+  {
+    "revision": "71d9fd2b2fc994309522",
+    "url": "/rmn-brtshk-resume/static/css/main.b6744ed6.chunk.css"
+  },
+  {
+    "revision": "d967258ba2a98a6a71a7",
+    "url": "/rmn-brtshk-resume/static/js/2.6c274de4.chunk.js"
+  },
+  {
+    "revision": "2e6bffa6107ad3258e68",
+    "url": "/rmn-brtshk-resume/static/js/3.58d24c7a.chunk.js"
+  },
+  {
+    "revision": "71d9fd2b2fc994309522",
+    "url": "/rmn-brtshk-resume/static/js/main.86f9481b.chunk.js"
+  },
+  {
+    "revision": "a901de6eb3bb7e7bda98",
+    "url": "/rmn-brtshk-resume/static/js/runtime~main.66b0f1c1.js"
+  },
+  {
+    "revision": "71e4687820b6c3539a35a976487150cd",
+    "url": "/rmn-brtshk-resume/static/media/back.71e46878.jpg"
+  }
+]);
